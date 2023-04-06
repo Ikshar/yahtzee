d11cf8af9d6a87c58b47fa366d8aeab760f756d0
Ikshar <mainhardor@gmail.com>
2023-04-06 18:59:52 +0300

    extract styles

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 import "./styles/styles.scss";
 import "./styles/layout.scss";
 import "./styles/scoreTable.scss";
+import "./styles/dice.scss";
+import "./styles/rollButton.scss";
+import "./styles/statusBar.scss";
 import { App } from "./App";
 
 createRoot(document.getElementById("root"))
